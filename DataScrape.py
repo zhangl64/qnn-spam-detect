1a52435315cf01631bb00d38b2e4633623c97d13
Ainaz Jamshidi <47214448+ajam74001@users.noreply.github.com>
2025-03-05 15:40:14 -0500

    Add files via upload

---
+++ DataScrape.py
@@ -0,0 +1,7 @@
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("imdeepmind/preprocessed-trec-2007-public-corpus-dataset")
+
+print("Path to dataset files:", path)
+# /home/ainazj1/.cache/kagglehub/datasets/imdeepmind/preprocessed-trec-2007-public-corpus-dataset/versions/1

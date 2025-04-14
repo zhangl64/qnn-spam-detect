1d82f8a461642936cffe8b4a96fb7ab9841df2f0
slrrla <66596548+slrrla@users.noreply.github.com>
2025-04-13 22:28:23 -0400

    commit

--- DataScrape.py
+++ DataScrape.py
@@ -1,7 +1,5 @@
 import kagglehub
 
-# Download latest version
 path = kagglehub.dataset_download("imdeepmind/preprocessed-trec-2007-public-corpus-dataset")
 
 print("Path to dataset files:", path)
-# /home/ainazj1/.cache/kagglehub/datasets/imdeepmind/preprocessed-trec-2007-public-corpus-dataset/versions/1

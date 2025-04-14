af93ee220b1ca6cd9d41754937257538e9451eec
slrrla <66596548+slrrla@users.noreply.github.com>
2025-04-14 13:19:40 -0400

    update_versions for requirement.txt

---
+++ versions.py
@@ -0,0 +1,13 @@
+import pennylane as qml
+import torch
+import sklearn
+import wandb
+import numpy as np
+import pandas as pd
+
+print("pennylane:", qml.__version__)
+print("torch:", torch.__version__)
+print("scikit-learn:", sklearn.__version__)
+print("wandb:", wandb.__version__)
+print("numpy:", np.__version__)
+print("pandas:", pd.__version__)
